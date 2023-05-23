085a96aeee6d4b5e5991a5ebd2499d61c40f0fe8
Anatolij Goga <anzefal@gmail.com>
2023-05-23 20:14:15 +0300

    Minor fix and added updateStatusContact
    Fixed HttpErrors in to class. Added "updateStatusContact" function.

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,39 +1,49 @@
-const contacts = require("../models/contacts");
-
 const { HttpError, controlWrapper } = require("../helpers");
+const { Contact } = require("../models/contact");
 
 const listContacts = async (req, res) => {
-  const result = await contacts.listContacts();
+  const result = await Contact.listContacts();
   res.status(200).json(result);
 };
 
 const getContactById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.getContactById(contactId);
+  const result = await Contact.getContactById(contactId);
 
-  if (!result) throw HttpError(404, "Not found");
+  if (!result) throw new HttpError(404, "Not found");
   res.status(200).json(result);
 };
 
 const addContact = async (req, res) => {
-  const result = await contacts.addContact(req.body);
+  const result = await Contact.addContact(req.body);
   res.status(201).json(result);
 };
 
 const removeContact = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.removeContact(contactId);
+  const result = await Contact.removeContact(contactId);
 
-  if (!result) throw HttpError(404, "Not found");
+  if (!result) throw new HttpError(404, "Not found");
 
   res.status(200).json({ message: "Contact deleted" });
 };
 
 const updateContact = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.updateContact(contactId, req.body);
+  const result = await Contact.updateContact(contactId, req.body);
 
-  if (!result) throw HttpError(404, "Not found");
+  if (!result) throw new HttpError(404, "Not found");
+
+  res.status(200).json(result);
+};
+
+const updateStatusContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+
+  if (!result) throw new HttpError(404, "Not found");
 
   res.status(200).json(result);
 };
@@ -44,4 +54,5 @@ module.exports = {
   addContact: controlWrapper(addContact),
   removeContact: controlWrapper(removeContact),
   updateContact: controlWrapper(updateContact),
+  updateStatusContact: controlWrapper(updateStatusContact),
 };
