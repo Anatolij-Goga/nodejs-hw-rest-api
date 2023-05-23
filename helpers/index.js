8fc26fc0a08afe2a760325dc52fbf81129dbcdc4
Anatolij Goga <anzefal@gmail.com>
2023-05-23 20:16:34 +0300

    Exported handleMongooseError

--- helpers/index.js
+++ helpers/index.js
@@ -1,4 +1,5 @@
 const HttpError = require("./HttpError");
 const controlWrapper = require("./controlWrapper");
+const handleMongooseError = require("./handleMongooseError");
 
-module.exports = { HttpError, controlWrapper };
+module.exports = { HttpError, controlWrapper, handleMongooseError };
