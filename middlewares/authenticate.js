5c5096450c8278791d4528bceed15d3d57029752
Anatolij Goga <anzefal@gmail.com>
2023-05-28 19:00:36 +0300

    Added authenticate
    Added authentication / JWT check

---
+++ middlewares/authenticate.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const { HttpError } = require("../helpers");
+
+const { SECRET_KEY } = process.env;
+const { User } = require("../models/userMongoose");
+
+const authenticate = async (req, _, next) => {
+  const authError = new HttpError(401, "Not authorized");
+
+  const { authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+
+  if (bearer !== "Bearer") {
+    next(authError);
+  }
+
+  try {
+    const { id } = jwt.verify(token, SECRET_KEY);
+    const user = await User.findById(id);
+
+    if (!user || !user.token || user.token !== token) {
+      next(authError);
+    }
+
+    req.user = user;
+    next();
+  } catch {
+    next(authError);
+  }
+};
+
+module.exports = authenticate;
